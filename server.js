4a319e30458110ee36e82244b1f5f4258b358e2d
Cortney-Logan <logan.cort@gmail.com)>
2021-03-10 14:52:11 -0500

    initial clean up of unneeded files

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+
+//pulls in username and password from .env
+//note that naming convention is all caps for .env variables
+const user = process.env.USER
+const password = process.env.PASSWORD

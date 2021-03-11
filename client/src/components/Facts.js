72f025ca900104170e95439cfef548d3b46f4a7d
Cortney-Logan <logan.cort@gmail.com)>
2021-03-11 09:07:29 -0500

    refactor fetch and data population on facts page

--- client/src/components/Facts.js
+++ client/src/components/Facts.js
@@ -11,31 +11,32 @@ function Facts(props) {
       fetch("/allposts")
         .then((res) => res.json())
         //sets resulting array in state as entries
-        .then((entryInfo) => setEntries(entryInfo));
+        .then((entryInfo) => {
+          let resultsArray = [];
+          entryInfo.forEach((post) => {
+            resultsArray.push(post);
+          });
+          setEntries(resultsArray);
+        });
     }
   });
 
-  //stores entries into an array for presentation on page
-  let listOfEntries = [];
-
-  entries &&
-    entries.forEach((post) => {
-      listOfEntries.push(post);
-    });
+  console.log("entries is", entries);
 
   return (
     <div id="facts-page-container">
       <h1>This is the facts page - for all entries</h1>
       <div id="list-of-entries">
-        {listOfEntries.map((entry,index) => {
-          return (
-            <div key={index}>
-              <h5>{entry.title}</h5>
-              <h6>{entry.date}</h6>
-              <p>{entry.content}</p>
-            </div>
-          );
-        })}
+        {entries &&
+          entries.map((entry, index) => {
+            return (
+              <div key={index}>
+                <h5>{entry.title}</h5>
+                <h6>{entry.date}</h6>
+                <p>{entry.content}</p>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
